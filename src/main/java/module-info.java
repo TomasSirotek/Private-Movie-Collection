147ad76c7f955fe0eefd27568c5c365e089c34d4
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-01-05 11:47:35 +0100

    Create ConnectionManager.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.movie_collection.private_movie_collection {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires com.microsoft.sqlserver.jdbc;
+    requires java.naming;
 
     exports com.movie_collection.gui.controllers;
     opens com.movie_collection.gui.controllers to javafx.fxml;
