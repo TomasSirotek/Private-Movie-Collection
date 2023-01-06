46c28c5380f6a1290272f7485d05c719f792489c
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-01-06 15:00:10 +0100

    implement movie service

--- src/main/java/com/movie_collection/bll/services/interfaces/IMovieService.java
+++ src/main/java/com/movie_collection/bll/services/interfaces/IMovieService.java
@@ -1,11 +1,27 @@
 package com.movie_collection.bll.services.interfaces;
 
+import com.movie_collection.be.Movie;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IMovieService {
     /**
      * method tyo get list of all movies
+     *
      * @return List<Movie> to be created
      */
-    List<String> getAllMovies();
+    List<Movie> getAllMovies() throws SQLException;
+
+    /**
+     * method to create a movie
+     *
+     * @param movie to be created
+     * @return created movie
+     */
+    int createMovie(Movie movie) throws SQLException;
+    int updateMovie(Movie movie) throws SQLException;
+    int deleteMovie(int id) throws SQLException;
+    Movie getMovieById(int id) throws SQLException;
+    List<Movie> getAllMoviesInTheCategory(int categoryId) throws SQLException;
 }
