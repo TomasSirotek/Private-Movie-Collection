26443fe74d07f2cc9da95e2c9549c68b0dc14daa
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-01-06 18:59:57 +0100

    change method order in code

--- src/main/java/com/movie_collection/dal/dao/MovieDAO.java
+++ src/main/java/com/movie_collection/dal/dao/MovieDAO.java
@@ -14,60 +14,6 @@ import java.util.List;
 public class MovieDAO implements IMovieDAO {
     private static final ConnectionManager cm = new ConnectionManager();
 
-    public int createMovie(Movie movie) throws SQLException {
-        try (Connection con = cm.getConnection()) {
-            String sql = "INSERT INTO Movie (name, rating, path, lastview) VALUES (?, ?, ?, ?)";
-            PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            pstmt.setString(1, movie.name().get());
-            pstmt.setDouble(2, movie.rating());
-            pstmt.setString(3, movie.path().get());
-            pstmt.setDate(4, movie.lastview());
-            return pstmt.executeUpdate();
-        }
-    }
-
-    public int deleteMovie(int id) throws SQLException {
-        try (Connection con = cm.getConnection()) {
-            String sql = "DELETE FROM CatMovie WHERE Movieid = ?";
-            PreparedStatement pstmt = con.prepareStatement(sql);
-            pstmt.setInt(1, id);
-            int rs1 = pstmt.executeUpdate();
-            String sql2 = "DELETE FROM Movie WHERE id = ?";
-            PreparedStatement pstmt2 = con.prepareStatement(sql2);
-            pstmt2.setInt(1, id);
-            int rs2 = pstmt2.executeUpdate();
-            return rs1 + rs2;
-        }
-    }
-
-    public int updateMovie(Movie movie) throws SQLException {
-        try (Connection con = cm.getConnection()) {
-            String sql = "UPDATE Movie SET name = ?, rating = ?, path = ?, lastview = ? WHERE id = ?";
-            PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            pstmt.setString(1, movie.name().get());
-            pstmt.setDouble(2, movie.rating());
-            pstmt.setString(3, movie.path().get());
-            pstmt.setDate(4, movie.lastview());
-            pstmt.setInt(5, movie.id());
-            return pstmt.executeUpdate();
-        }
-    }
-
-    public Movie getMovieById(int id) throws SQLException {
-        try (Connection con = cm.getConnection()) {
-            String sql = "SELECT * FROM Movie WHERE id = ?";
-            PreparedStatement pstmt = con.prepareStatement(sql);
-            pstmt.setInt(1, id);
-            ResultSet rs = pstmt.executeQuery();
-            rs.next();
-            StringProperty name = new SimpleStringProperty(rs.getString("name"));
-            double rating = rs.getDouble("rating");
-            StringProperty path = new SimpleStringProperty(rs.getString("path"));
-            Date lastview = rs.getDate("lastview");
-            return new Movie(id, name, rating, path, getCategoriesOfMovie(id), lastview);
-        }
-    }
-
     public List<Movie> getAllMovies() throws SQLException {
         List<Movie> movies = new ArrayList<>();
         try (Connection con = cm.getConnection()){
@@ -89,14 +35,14 @@ public class MovieDAO implements IMovieDAO {
     public List<Movie> getAllMoviesInTheCategory(int categoryId) throws SQLException {
         ArrayList<Movie> movies = new ArrayList<>();
         try (Connection connection = cm.getConnection()) {
-            String sql = "SELECT * FROM Movie INNER JOIN CatMovie ON Movie.id = CatMovie.MovieId WHERE CatMovie.CategoryId = ?";
+            String sql = "SELECT (Movie.id, Movie.title, Movie.rating, Movie.path, Movie.lastview, CatMovie.MovieId, CatMovie.CategoryId) FROM Movie INNER JOIN CatMovie ON Movie.id = CatMovie.MovieId WHERE CatMovie.CategoryId = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, categoryId);
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
                 int id = rs.getInt("id");
                 StringProperty title = new SimpleStringProperty(rs.getString("title"));
-                double rating = rs.getDouble("imdbRating");
+                double rating = rs.getDouble("rating");
                 StringProperty path = new SimpleStringProperty(rs.getString("path"));
                 Date lastview = rs.getDate("lastview");
                 movies.add(new Movie(id, title, rating, path, getCategoriesOfMovie(id), lastview));
@@ -105,10 +51,25 @@ public class MovieDAO implements IMovieDAO {
         return movies;
     }
 
-    public List<Category> getCategoriesOfMovie(int MovieId) throws SQLException {
+    public Movie getMovieById(int id) throws SQLException {
+        try (Connection con = cm.getConnection()) {
+            String sql = "SELECT * FROM Movie WHERE id = ?";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            StringProperty name = new SimpleStringProperty(rs.getString("name"));
+            double rating = rs.getDouble("rating");
+            StringProperty path = new SimpleStringProperty(rs.getString("path"));
+            Date lastview = rs.getDate("lastview");
+            return new Movie(id, name, rating, path, getCategoriesOfMovie(id), lastview);
+        }
+    }
+
+    private List<Category> getCategoriesOfMovie(int MovieId) throws SQLException {
         ArrayList<Category> allCategories = new ArrayList<>();
         try (Connection con = cm.getConnection()) {
-            String sql = "SELECT * FROM Category JOIN CatMovie ON Category.id = CatMovie.CategoryId WHERE MovieId = ?";
+            String sql = "SELECT (Category.id, Category.name, CatMovie.CategoryId, CatMovie.MovieId) FROM Category JOIN CatMovie ON Category.id = CatMovie.CategoryId WHERE MovieId = ?";
             PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setInt(1, MovieId);
             ResultSet rs = pstmt.executeQuery();
@@ -119,5 +80,44 @@ public class MovieDAO implements IMovieDAO {
             }
         }
         return allCategories;
+    }
+
+    public int createMovie(Movie movie) throws SQLException {
+        try (Connection con = cm.getConnection()) {
+            String sql = "INSERT INTO Movie (name, rating, path, lastview) VALUES (?, ?, ?, ?)";
+            PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, movie.name().get());
+            pstmt.setDouble(2, movie.rating());
+            pstmt.setString(3, movie.path().get());
+            pstmt.setDate(4, movie.lastview());
+            return pstmt.executeUpdate();
+        }
+    }
+
+    public int updateMovie(Movie movie) throws SQLException {
+        try (Connection con = cm.getConnection()) {
+            String sql = "UPDATE Movie SET name = ?, rating = ?, path = ?, lastview = ? WHERE id = ?";
+            PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, movie.name().get());
+            pstmt.setDouble(2, movie.rating());
+            pstmt.setString(3, movie.path().get());
+            pstmt.setDate(4, movie.lastview());
+            pstmt.setInt(5, movie.id());
+            return pstmt.executeUpdate();
+        }
+    }
+
+    public int deleteMovie(int id) throws SQLException {
+        try (Connection con = cm.getConnection()) {
+            String sql = "DELETE FROM CatMovie WHERE Movieid = ?";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, id);
+            int rs1 = pstmt.executeUpdate();
+            String sql2 = "DELETE FROM Movie WHERE id = ?";
+            PreparedStatement pstmt2 = con.prepareStatement(sql2);
+            pstmt2.setInt(1, id);
+            int rs2 = pstmt2.executeUpdate();
+            return rs1 + rs2;
+        }
     }
 }
--- src/main/java/com/movie_collection/dal/interfaces/IMovieDAO.java
+++ src/main/java/com/movie_collection/dal/interfaces/IMovieDAO.java
@@ -7,44 +7,10 @@ import java.util.List;
 
 public interface IMovieDAO {
     /**
-     * Creates a new movie in the database with given properties
-     *
-     * @param movie an object of type Movie with necessary properties
-     * @return number of rows affected
-     * @throws SQLException if the connection to the database fails
-     */
-    int createMovie(Movie movie) throws SQLException;
-
-    /**
-     * Removes a movie from the database based on the id
-     *
-     * @param id id of movie to be deleted
-     * @return number of rows affected
-     * @throws SQLException if the connection to the database fails
-     */
-    int deleteMovie(int id) throws SQLException;
-
-    /**
-     * Updates a movie in the database depending on the id of the movie
-     *
-     * @param movie a movie object with the new values
-     * @return number of rows affected
-     * @throws SQLException if the connection to the database fails
-     */
-    int updateMovie(Movie movie) throws SQLException;
-
-    /**
-     * @param id integer of the movie id
-     * @return returns a movie object with the id
-     * @throws SQLException if the connection to the database fails
-     */
-    Movie getMovieById(int id) throws SQLException;
-
-    /**
-     * Class responsible for getting all movies from the database
-     *
+    * Class responsible for getting all movies from the database
+    *
      * @return List of all movies
-     * @throws SQLException if the connection to the database fails
+    * @throws SQLException if the connection to the database fails
      */
     List<Movie> getAllMovies() throws SQLException;
 
@@ -56,4 +22,38 @@ public interface IMovieDAO {
      * @throws SQLException if the connection to the database fails
      */
     List<Movie> getAllMoviesInTheCategory(int categoryId) throws SQLException;
+
+    /**
+     * @param id integer of the movie id
+     * @return returns a movie object with the id
+     * @throws SQLException if the connection to the database fails
+     */
+    Movie getMovieById(int id) throws SQLException;
+
+    /**
+     * Creates a new movie in the database with given properties
+     *
+     * @param movie an object of type Movie with necessary properties
+     * @return number of rows affected
+     * @throws SQLException if the connection to the database fails
+     */
+    int createMovie(Movie movie) throws SQLException;
+
+    /**
+     * Updates a movie in the database depending on the id of the movie
+     *
+     * @param movie a movie object with the new values
+     * @return number of rows affected
+     * @throws SQLException if the connection to the database fails
+     */
+    int updateMovie(Movie movie) throws SQLException;
+
+    /**
+     * Removes a movie from the database based on the id
+     *
+     * @param id id of movie to be deleted
+     * @return number of rows affected
+     * @throws SQLException if the connection to the database fails
+     */
+    int deleteMovie(int id) throws SQLException;
 }
