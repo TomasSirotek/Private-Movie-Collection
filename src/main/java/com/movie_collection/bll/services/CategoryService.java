42500d43000167f249bb8ba523c29afa12e737c5
Nicola Martinez <nicoenrique_1994@hotmail.com>
2023-01-08 19:47:37 +0100

    fixing minor issue in deletion method

--- src/main/java/com/movie_collection/bll/services/CategoryService.java
+++ src/main/java/com/movie_collection/bll/services/CategoryService.java
@@ -1,29 +1,36 @@
 package com.movie_collection.bll.services;
 
+import com.google.inject.Inject;
 import com.movie_collection.be.Category;
 import com.movie_collection.bll.services.interfaces.ICategoryService;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import com.movie_collection.dal.interfaces.ICategoryDAO;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.sql.SQLException;
 import java.util.List;
+
 
 public class CategoryService implements ICategoryService {
 
+    private final ICategoryDAO categoryDAO;
+
+    @Inject
+    public CategoryService(ICategoryDAO categoryDAO) {
+        this.categoryDAO = categoryDAO;
+    }
     @Override
-    public List<Category> getAllCategories() {
-        return list;
+    public List<Category> getAllCategories() throws SQLException {
+        return categoryDAO.getAllCategories();
     }
 
     @Override
-    public int createCategory(Category category) {
-        list.add(category);
+    public int createCategory(Category category) throws SQLException {
+        categoryDAO.addCategory(category);
         return 0;
     }
 
     @Override
-    public int deleteCategory(int id) {
-        return list.removeIf(x -> x.id() == id);
+    public int deleteCategory(int id) throws SQLException {
+        return categoryDAO.deleteCategory(id);
+        //return list.removeIf(x -> x.id() == id);
     }
 }
