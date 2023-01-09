86da0bdeb820a73841ed085db006e363a8a34401
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-01-09 20:47:28 +0100

    fix creating movies assigns categories

--- src/main/java/com/movie_collection/bll/services/MovieService.java
+++ src/main/java/com/movie_collection/bll/services/MovieService.java
@@ -54,7 +54,7 @@ public class MovieService implements IMovieService {
 
     private Movie linkingCategoriesToId(Movie movie) throws SQLException {
         ArrayList<Category> allCategories = new ArrayList<>(categoryService.getAllCategories());
-        List<Category> categories = movie.categories();
+        List<Category> categories = new ArrayList<>(movie.categories());
         for (int i = 0; i < categories.size(); i++) {
             String catName = categories.get(i).name().get();
             Category category = allCategories.stream().filter(c -> c.name().get().equals(catName)).findFirst().orElse(new Category(0, new SimpleStringProperty("Does not exist")));
