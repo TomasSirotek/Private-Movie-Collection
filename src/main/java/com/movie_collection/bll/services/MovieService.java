745d52d8ef41b4a8f1590e9bd6846727f5062022
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-01-09 19:46:51 +0100

    separate the Category Conversion method

--- src/main/java/com/movie_collection/bll/services/MovieService.java
+++ src/main/java/com/movie_collection/bll/services/MovieService.java
@@ -29,19 +29,12 @@ public class MovieService implements IMovieService {
 
     @Override
     public int createMovie(Movie movie) throws SQLException {
-        ArrayList<Category> allCategories = new ArrayList<>(categoryService.getAllCategories());
-        List<Category> categories = movie.categories();
-        for (int i = 0; i < categories.size(); i++) {
-            String catName = categories.get(i).name().get();
-            Category category = allCategories.stream().filter(c -> c.name().get().equals(catName)).findFirst().orElse(new Category(0, new SimpleStringProperty("Does not exist")));
-            categories.set(i, category);
-        }
-        return movieDAO.createMovie(new Movie(movie.id(), movie.name(), movie.rating(), movie.absolutePath(), categories, movie.lastview()));
+        return movieDAO.createMovie(linkingCategoriesToId(movie));
     }
 
     @Override
     public int updateMovie(Movie movie) throws SQLException {
-        return movieDAO.updateMovie(movie);
+        return movieDAO.updateMovie(linkingCategoriesToId(movie));
     }
 
     @Override
@@ -57,5 +50,16 @@ public class MovieService implements IMovieService {
     @Override
     public List<Movie> getAllMoviesInTheCategory(int categoryId) throws SQLException {
         return movieDAO.getAllMoviesInTheCategory(categoryId);
+    }
+
+    private Movie linkingCategoriesToId(Movie movie) throws SQLException {
+        ArrayList<Category> allCategories = new ArrayList<>(categoryService.getAllCategories());
+        List<Category> categories = movie.categories();
+        for (int i = 0; i < categories.size(); i++) {
+            String catName = categories.get(i).name().get();
+            Category category = allCategories.stream().filter(c -> c.name().get().equals(catName)).findFirst().orElse(new Category(0, new SimpleStringProperty("Does not exist")));
+            categories.set(i, category);
+        }
+        return new Movie(movie.id(), movie.name(), movie.rating(), movie.absolutePath(), categories, movie.lastview());
     }
 }
