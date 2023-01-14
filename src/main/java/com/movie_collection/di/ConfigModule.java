a9f9e7f333174ff7044c9e0da8dd1966352cac67
TomassSimko <simko.t@email.cz>
2023-01-14 15:42:43 +0100

    åproperly fixed bus and injection to be correct

--- src/main/java/com/movie_collection/di/ConfigModule.java
+++ src/main/java/com/movie_collection/di/ConfigModule.java
@@ -15,8 +15,6 @@ import com.movie_collection.dal.dao.CategoryDAO;
 import com.movie_collection.dal.dao.MovieDAO;
 import com.movie_collection.dal.interfaces.ICategoryDAO;
 import com.movie_collection.dal.interfaces.IMovieDAO;
-import com.movie_collection.gui.controllers.BaseController;
-import com.movie_collection.gui.controllers.MovieController;
 import com.movie_collection.gui.controllers.controllerFactory.ControllerFactory;
 import com.movie_collection.gui.controllers.controllerFactory.IControllerFactory;
 import com.movie_collection.gui.models.CategoryModel;
@@ -28,18 +26,23 @@ public class ConfigModule extends AbstractModule {
     @Override
     public void configure() {
 
-        /*
-         * Bind the MovieDAO interface to the implementation
-         */
-        bind(IMovieDAO.class).to(MovieDAO.class);
-        /*
-         * Bind the CategoryDAO interface to the implementation
-         */
-        bind(ICategoryDAO.class).to(CategoryDAO.class);
+        /* *************************************************************************
+         *                                                                         *
+         * CONTROLLER                                                                  *
+         *                                                                         *
+         **************************************************************************
+
         /*
          * Injection of binding
          */
         bind(IControllerFactory.class).to(ControllerFactory.class);
+
+
+        /* *************************************************************************
+         *                                                                         *
+         * SERVICE                                                                 *
+         *                                                                         *
+         **************************************************************************
 
         /*
          * Injection of movie service
@@ -49,22 +52,7 @@ public class ConfigModule extends AbstractModule {
         /*
          * Injection of movie service
          */
-        bind(ICategoryService.class).to(CategoryService.class).in(Singleton.class);
-
-        /*
-         * Injection of movie service
-         */
-        bind(ICategoryModel.class).to(CategoryModel.class).in(Singleton.class);
-
-        /*
-         * Injection of movie model
-         */
-        bind(IMovieModel.class).to(MovieModel.class);
-        /*
-         * Injection of Filter service
-         */
-        bind(IFilter.class).to(Filter.class);
-
+        bind(ICategoryService.class).to(CategoryService.class);
 
         /*
          * Binds api service
@@ -72,10 +60,58 @@ public class ConfigModule extends AbstractModule {
 
         bind(IAPIService.class).to(APIService.class);
 
+        /* *************************************************************************
+        *                                                                         *
+        * MODEL                                                                   *
+        *                                                                         *
+        **************************************************************************
+
+        /*
+         * Injection of movie service
+         * Singleton due to the usability and to have just one instance
+         */
+        bind(ICategoryModel.class).to(CategoryModel.class).in(Singleton.class);
+
+        /*
+         * Injection of movie model
+         */
+        bind(IMovieModel.class).to(MovieModel.class).in(Singleton.class);
+
+        /* *************************************************************************
+        *                                                                         *
+        * DAO                                                                     *
+        *                                                                         *
+        **************************************************************************
+        /*
+         * Bind the MovieDAO interface to the implementation
+         */
+        bind(IMovieDAO.class).to(MovieDAO.class);
+        /*
+         * Bind the CategoryDAO interface to the implementation
+         */
+        bind(ICategoryDAO.class).to(CategoryDAO.class);
+
+        /* *************************************************************************
+        *                                                                         *
+        * EVENT                                                                   *
+        *                                                                         *
+        **************************************************************************
+
         /*
          * Bind even bus as in singleton scope
+         * As eager singleton to ensure instantiation asap Injector is created
          */
-        bind(EventBus.class).in(Singleton.class);
+        bind(EventBus.class).asEagerSingleton();
 
+        /* *************************************************************************
+        *                                                                         *
+        * HELPER                                                                   *
+        *                                                                         *
+        **************************************************************************
+
+        /*
+         * Injection of Filter helper
+         */
+        bind(IFilter.class).to(Filter.class);
     }
 }
